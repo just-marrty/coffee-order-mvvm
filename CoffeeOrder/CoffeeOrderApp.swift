a67a0ce3835f1382cd852c84acc9609342685918
Martin <marrty.h.1992@gmail.com>
2025-12-16 14:52:53 +0100

    Initial Commit

---
+++ CoffeeOrder/CoffeeOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderApp.swift
+//  CoffeeOrder
+//
+//  Created by Martin Hrbáček on 16.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
